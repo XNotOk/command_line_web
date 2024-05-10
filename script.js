5a58bae0fa186019b89ad81c7f73e583049311bc
XNotOk <okoduwajosiah@gmail.com>
2024-05-10 22:51:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,10 +9,20 @@ fetch('command_exec.wasm')
         // Function to execute the command
         window.executeCommand = () => {
             const command = document.getElementById("commandInput").value;
-            const result = executeCommand(command);
+            const resultPtr = executeCommand(allocateString(obj, command));
+            const result = UTF8ToString(resultPtr);
             document.getElementById("result").innerText = result;
+            _free(resultPtr); // Free memory allocated by WebAssembly
         };
     })
     .catch(error => {
         console.error('Error loading WebAssembly module:', error);
     });
+
+// Utility function to allocate a string in WebAssembly memory
+function allocateString(obj, str) {
+    const length = lengthBytesUTF8(str) + 1;
+    const ptr = obj.instance.exports.allocate(length);
+    stringToUTF8(str, ptr, length);
+    return ptr;
+}
